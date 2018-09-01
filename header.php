8d3e8589b24ce847381bfcf2b5198932fdbc8260
ProgramadorWebTI <programadorwebti@gmail.com>
2018-09-01 15:25:10 -0300

    Cabeçalho

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+require_once "vendor/autoload.php";
+
+use App\Helpers;
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <link rel="stylesheet" href="<?php echo Helpers::base_url('public/node_modules/bootstrap/dist/css/bootstrap.min.css'); ?>">
+    </head>
+    <body>
