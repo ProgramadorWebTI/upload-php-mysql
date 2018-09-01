451805912032c94fff0f6a44a120064c136041cb
ProgramadorWebTI <programadorwebti@gmail.com>
2018-09-01 15:29:45 -0300

    modificação constantes

--- index.php
+++ index.php
@@ -1,28 +1,13 @@
-<?php
-require_once "vendor/autoload.php";
-
-use App\Helpers;
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <link rel="stylesheet" href="<?php echo Helpers::base_url('public/node_modules/bootstrap/dist/css/bootstrap.min.css'); ?>">
-    </head>
-    <body>
-        <div class="container mt-5">
-            <form method="post" enctype="multipart/form-data">
-                <div class="form-group" >
-                    <input type="text" name="nome" class="form-control" />
-                </div>
-                <div class="form-group">
-                    <input type="file" name="nome" class="form-control" />
-                </div>
-                <button type="submit" class="btn btn-primary">enviar</button>
-            </form>
+<?php require_once './header.php'; ?>
+<div class="container mt-5">
+    <form method="post" enctype="multipart/form-data" action="gestao.php?q=add">
+        <div class="form-group" >
+            <input type="text" name="nome" class="form-control" />
         </div>
-    </body>
-</html>
+        <div class="form-group">
+            <input type="file" name="arquivo" class="form-control" />
+        </div>
+        <button type="submit" class="btn btn-primary">enviar</button>
+    </form>
+</div>
+<?php require_once './footer.php'; ?>
